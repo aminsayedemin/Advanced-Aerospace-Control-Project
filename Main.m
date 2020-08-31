a1dd3ce4dcd96ff655f0eca124fbd3c1da5da814
Davide Selvatici <davide.selvatici@mail.polimi.it>
2020-08-31 09:38:45 +0200

    31/8

---
+++ Main.m
@@ -0,0 +1,287 @@
+clear all; close all; clc;
+% Attitude Control System for ANT-R UAV
+% Group: Romagnoli, Sayed, Selvatici
+%% Section: Main
+
+%% Initial Data
+Ts = 0.004; % Sampling interval
+g = 9.81;
+
+%% Lateral Dynamics Block
+% x = [v p \phi]^T; y = [p \phi]^T
+Y_v = ureal('yv', -0.264,'Perc', 4.837);
+Y_p = 0;
+L_v = ureal('lv', -7.349,'Perc', 4.927);
+L_p = 0;
+Y_d = ureal('yd', 9.568,'Perc', 4.647);
+L_d = ureal('ld', 1079.339,'Perc', 2.762);
+
+A = [Y_v    Y_p     g;
+    L_v     L_p     0;
+    0       1       0];
+N = size(A);
+Anom = A.NominalValue; % The matrix A with its nominal value
+
+B = [Y_d;
+    L_d;
+    0];
+Bnom = B.NominalValue; % The vector B with its nominal value
+
+C = [0      1       0;
+    0       0       1];
+
+D = [0;
+    0];
+
+%% Nominal plant
+ld = ss(Anom, Bnom, C, D);
+ld_dis = c2d(ld, Ts, 'foh');
+
+G = tf(ld_dis);
+G.u = {'delta_lat'};
+G.y = {'p', 'phi'};
+
+%% Uncertain plant
+ld_un_dis = ss(A, B, C, D, Ts);
+G_un = tf(ld_un_dis);
+
+%% Controller: R_p
+b = realp('b', 1);
+c1 = realp('c1',1);
+c2 = realp('c2',1);
+d1 = realp('d1',1);
+d2 = realp('d2',1);
+
+Ap = [1 0; 0 0];
+Bp = [b -b; 0 0.5];
+Cp = [c1 c2];
+Dp = [d1 d2];
+
+Rp = ss(Ap, Bp, Cp, Dp, Ts);
+
+Rp.u = {'p_0', 'p'};
+Rp.y = {'delta_lat'};
+
+%% Controller: R_phi
+d3 = realp('d3', 1);
+Dphi = [d3];
+
+Rphi = ss(0, 0, 0, Dphi, Ts);
+Rphi.u = {'e_phi'};
+Rphi.y = {'p_0'};
+
+%% Weights for "hinfstruct"
+% Weight on the sensitivity function
+csi_w1 = 0.9;
+om_w1 = 10; %crossover frequency
+s = zpk('s');
+F_w1 = om_w1^2/(s^2 + 2*csi_w1*om_w1*s + om_w1^2);
+W1inv = c2d((1 - F_w1), Ts, 'foh');
+W1 = 1/W1inv;
+W1.u = {'e_phi'};
+W1.y = {'z_1'};
+
+% Weight on the control sensitivity
+W2inv = tf([1],[1/30 1]);
+W2inv = c2d(W2inv, Ts, 'foh');
+W2 = 1/W2inv;
+W2.u = {'delta_lat'};
+W2.y = {'z_2'};
+
+%% Assembly
+Sum = sumblk('e_phi = phi_0 - phi');
+
+P_h = connect(Rp, W1, Sum, Rphi, G, {'phi_0'}, {'z_1'});
+% P_h = connect(Rp, W1, W2, Sum, Rphi, G, {'phi_0'}, {'z_1', 'z_2'});
+P_s = connect(G, Rp, Rphi, Sum, {'phi_0'}, {'p', 'phi'}, {'delta_lat'});
+
+%% Tuning
+% Desired dynamics
+csi = 0.9;
+om = 10;
+
+s = zpk('s');
+F0 = om^2/(s^2 + 2*csi*om*s + om^2);
+F0 = c2d(F0, Ts, 'foh');
+S0 = 1 - F0;
+
+soft = TuningGoal.Transient('phi_0', 'phi', F0, 'step');
+hard = TuningGoal.Gain('phi_0', 'delta_lat', 1);
+
+starts = 50;
+
+% options = systuneOptions;
+% options.RandomStart = starts;
+% [K_s, GSOFT, GHARD, INFO] = systune(P_s, soft, hard, options); % Soft: Objective, Hard: Constraint
+% % GSOFT, GHARD
+
+opt = hinfstructOptions('Display', 'final', 'RandomStart', starts);
+[K_h, gamma, info] = hinfstruct(P_h, opt);
+% gamma
+
+for i = 1:2 % Needed for the first plot
+    if i == 1
+        K = K_h;
+%     elseif i == 2
+%         K = K_s;
+    end
+    
+% [K.Blocks.b.Value; K.Blocks.c1.Value; K.Blocks.c2.Value;
+%     K.Blocks.d1.Value; K.Blocks.d2.Value; K.Blocks.d3.Value]
+
+%% Tuned system redefinition
+% Controller: R_p
+b = K.Blocks.b.Value;
+c1 = K.Blocks.c1.Value;
+c2 = K.Blocks.c2.Value;
+d1 = K.Blocks.d1.Value;
+d2 = K.Blocks.d2.Value;
+
+Ap =    [1  0;
+        0   0];
+    
+Bp =    [b  -b;
+        0   0.5];
+    
+Cp =    [c1 c2];
+
+Dp =    [d1 d2];
+
+Rp = ss(Ap, Bp, Cp, Dp, Ts);
+Rp.u = {'p_0', 'p'};
+Rp.y = {'delta_lat'};
+
+% Controller: R_phi
+d3 = K.Blocks.d3.Value;
+Dphi = d3;
+
+Rphi = ss(0, 0, 0, Dphi, Ts);
+Rphi.u = {'e_phi'};
+Rphi.y = {'p_0'};
+
+% Assembly
+Loop = connect(G, Rp, Rphi, Sum, {'phi_0'}, {'p', 'phi'});
+F = Loop(2); % Complementary Sensitivity
+S = connect(G, Rp, Rphi, Sum, {'phi_0'}, {'e_phi'}); % Sensitivity
+Q = connect(G, Rp, Rphi, Sum, {'phi_0'}, {'delta_lat'});% Control sensitivity
+
+if i == 1
+Loop_old = Loop;
+F_old = F; % F_old is the one obtained with hinfstruct
+S_old = S;
+Q_old = Q;
+end
+
+end
+
+%% Plots for "systune"
+inutili = 1; % Show (1) or hide (0) some graphs
+Tf = 3; % Final time of the step plots
+
+% Check the requirement
+% (here both hinfstruct and systune)
+csi = 0.9;
+om = 10;
+s = zpk('s');
+F0 = om^2/(s^2 + 2*csi*om*s + om^2);
+
+figure;
+hold on
+[y_s, t_Ls] = step(F, Tf); % Step of the system, with output phi
+[y_h, t_Lh] = step(F_old, Tf); % Step of the system, with output phi
+[z_s, t_F] = step(F0, Tf); % Step response of the desired dynamics
+
+h1 = plot(t_Ls, squeeze(y_s), 'LineWidth', 2);
+h2 = plot(t_Lh, squeeze(y_h), 'LineWidth', 2);
+h3 = plot(t_F, squeeze(z_s), '--', 'LineWidth', 2);
+
+% legend([h3(1), h1(1), h2(1)], 'Desired response', '\textit{systune}', '\textit{hinfstruct}', 'Location', 'southeast', 'Interpreter', 'Latex');
+% xlabel('Time [s]', 'Interpreter', 'Latex');
+% ylabel ('Amplitude', 'Interpreter', 'Latex');
+% axis ([0 Tf -0.2 1.2]);
+% grid on
+
+% Step response wrt phi and p
+figure;
+hold on
+[s_L1, t_L1] = step(Loop(1), Tf);
+[s_L2, t_L2] = step(Loop(2), Tf);
+
+subplot(2,1,1), plot(t_L1, squeeze(s_L1), 'LineWidth',2);
+xlabel('Time [s]', 'Interpreter', 'Latex');
+ylabel ('Amplitude', 'Interpreter', 'Latex');
+title('Output: p', 'Interpreter', 'Latex');
+axis ([0 Tf -0.2 4.5]);
+grid on
+
+subplot(2,1,2), plot(t_L2, squeeze(s_L2), 'LineWidth',2);
+xlabel('Time [s]', 'Interpreter', 'Latex');
+ylabel ('Amplitude', 'Interpreter', 'Latex');
+title('Output: phi', 'Interpreter', 'Latex');
+axis ([0 Tf -0.2 1.2]);
+grid on
+if inutili == 1
+    
+% Sensitivity
+figure;
+subplot(2,1,1), bode(S, S0);
+grid on;
+legend('S', 'Desired S', 'Interpreter', 'Latex');
+title ('Sensitivity Function', 'Interpreter', 'Latex');
+
+subplot(2,1,2), margin(S);
+grid on;
+
+% Complementary Sensitivity
+figure;
+subplot(2,1,1), bode(F, F0);
+grid on;
+legend('F', 'Desired F', 'Interpreter', 'Latex');
+title ('Complementary sensitivity Function', 'Interpreter', 'Latex');
+
+subplot(2,1,2), margin(F);
+grid on;
+end
+
+% Control Sensitivity
+figure;
+opt = stepDataOptions('StepAmplitude', deg2rad(10));
+[s_Q, t_Q] = step(Q, 50, opt);
+plot(t_Q, squeeze(s_Q), 'LineWidth',2)
+xlabel('Time', 'Interpreter', 'Latex');
+ylabel ('Amplitude', 'Interpreter', 'Latex');
+grid on;
+legend('Output: $\delta_{lat}$', 'Interpreter', 'Latex');
+title('Step of amplitude 10 response', 'Interpreter', 'Latex');
+
+% Control sensitivity function
+figure;
+bode(Q);
+grid on;
+title ('Control sensitivity Function', 'Interpreter', 'Latex');
+
+%% Plots for "hinfstruct"
+% Sensitivity
+figure;
+bode(S_old, W1inv, S0);
+legend('S', '1/W1', 'Desired S', 'Interpreter', 'Latex');
+title('hinfstruct');
+grid on
+
+% % Complementary Sensitivity
+% figure
+% bode(F, W3inv, F0);
+% legend('Loop', '1/W3', 'Desired F');
+% grid on;
+
+% figure;
+% bode(Loop(2) * W3)
+% title('Multiplication between F and W3');
+% grid on;
+
+% Control Sensitivity
+figure;
+bode(Q_old, W2inv);
+legend('Q', '1/W2');
+grid on;
+%% END
